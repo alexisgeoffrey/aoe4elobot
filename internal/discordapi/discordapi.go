de75da2c61b6b1f26be4cca5a4b8d97e3e4ad8cf
alexisgeoffrey <alexisgeoffrey1@gmail.com>
2022-03-07 17:41:31 -0600

    fixed assign to nil map

--- internal/discordapi/discordapi.go
+++ internal/discordapi/discordapi.go
@@ -124,6 +124,7 @@ func UpdateAllElo(s *discordgo.Session, guildId string) (string, error) {
 		if updatedElo[u.DiscordUserID] == nil {
 			us.Users[i].NewElo = u.OldElo
 		} else {
+			us.Users[i].NewElo = make(userElo, len(getEloTypes()))
 			for _, eloType := range getEloTypes() {
 				if elo, ok := updatedElo[u.DiscordUserID][eloType]; ok {
 					us.Users[i].NewElo[eloType] = elo
