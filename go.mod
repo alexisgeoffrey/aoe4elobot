27823c85ca935195de37eb7cbd53cfaee426d20d
alexisgeoffrey <alexisgeoffrey1@gmail.com>
2022-04-12 00:24:25 -0500

    fix temp errors

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/alexisgeoffrey/aoe4elobot
 go 1.18
 
 require (
-	github.com/alexisgeoffrey/aoe4api v0.2.1
 	github.com/bwmarrin/discordgo v0.24.0
 	github.com/ilyakaznacheev/cleanenv v1.2.6
 	github.com/jackc/pgtype v1.10.0
--- internal/discordapi/users.go
+++ internal/discordapi/users.go
@@ -1,134 +1,125 @@
 package discordapi
 
-import (
-	"fmt"
-	"sort"
-	"strings"
-
-	"github.com/alexisgeoffrey/aoe4api"
-	"github.com/bwmarrin/discordgo"
-)
-
 type users struct {
 	Users []user `json:"users"`
 }
 
-func (us users) updateAllEloRoles(s *discordgo.Session, guildId string) error {
-	reorder := false
-	for _, u := range us.Users {
-		member, err := s.State.Member(guildId, u.DiscordUserID)
-		if err != nil {
-			return fmt.Errorf("error retrieving guild member %s: %w", u.DiscordUserID, err)
-		}
-		roleSet := map[string]*discordgo.Role{}
-		for _, roleId := range member.Roles {
-			role, err := s.State.Role(guildId, roleId)
-			if err != nil {
-				return fmt.Errorf("error retrieving guild role %s: %w", roleId, err)
-			}
-			if strings.Contains(role.Name, "Elo:") {
-				roleSet[strings.ToLower(strings.Split(role.Name, " ")[0])] = role
-			}
-		}
+// func (us users) updateAllEloRoles(s *discordgo.Session, guildId string) error {
+// 	reorder := false
+// 	for _, u := range us.Users {
+// 		member, err := s.State.Member(guildId, u.DiscordUserID)
+// 		if err != nil {
+// 			return fmt.Errorf("error retrieving guild member %s: %w", u.DiscordUserID, err)
+// 		}
+// 		roleSet := map[string]*discordgo.Role{}
+// 		for _, roleId := range member.Roles {
+// 			role, err := s.State.Role(guildId, roleId)
+// 			if err != nil {
+// 				return fmt.Errorf("error retrieving guild role %s: %w", roleId, err)
+// 			}
+// 			if strings.Contains(role.Name, "Elo:") {
+// 				roleSet[strings.ToLower(strings.Split(role.Name, " ")[0])] = role
+// 			}
+// 		}
 
-		for _, eloType := range getEloTypes() {
-			if elo, ok := u.newElo[eloType]; ok && elo != u.oldElo[eloType] {
-				roleName := fmt.Sprintf("%s Elo: %s", strings.Title(eloType), elo)
-				if role, ok := roleSet[eloType]; ok {
-					_, err = s.GuildRoleEdit(guildId, role.ID, roleName, 1, false, 0, false)
-					if err != nil {
-						return fmt.Errorf("error editing guild role: %w", err)
-					}
-				} else {
-					role, err := s.GuildRoleCreate(guildId)
-					if err != nil {
-						return fmt.Errorf("error creating guild role: %w", err)
-					}
-					role, err = s.GuildRoleEdit(guildId, role.ID, roleName, 1, false, 0, false)
-					if err != nil {
-						return fmt.Errorf("error editing guild role: %w", err)
-					}
-					if err := s.GuildMemberRoleAdd(guildId, u.DiscordUserID, role.ID); err != nil {
-						return fmt.Errorf("error adding guild role: %w", err)
-					}
-					reorder = true
-				}
-			}
-		}
-	}
+// 		for _, eloType := range getEloTypes() {
+// 			if elo, ok := u.newElo[eloType]; ok && elo != u.oldElo[eloType] {
+// 				roleName := fmt.Sprintf("%s Elo: %s", strings.Title(eloType), elo)
+// 				if role, ok := roleSet[eloType]; ok {
+// 					_, err = s.GuildRoleEdit(guildId, role.ID, roleName, 1, false, 0, false)
+// 					if err != nil {
+// 						return fmt.Errorf("error editing guild role: %w", err)
+// 					}
+// 				} else {
+// 					role, err := s.GuildRoleCreate(guildId)
+// 					if err != nil {
+// 						return fmt.Errorf("error creating guild role: %w", err)
+// 					}
+// 					role, err = s.GuildRoleEdit(guildId, role.ID, roleName, 1, false, 0, false)
+// 					if err != nil {
+// 						return fmt.Errorf("error editing guild role: %w", err)
+// 					}
+// 					if err := s.GuildMemberRoleAdd(guildId, u.DiscordUserID, role.ID); err != nil {
+// 						return fmt.Errorf("error adding guild role: %w", err)
+// 					}
+// 					reorder = true
+// 				}
+// 			}
+// 		}
+// 	}
 
-	if reorder {
-		g, err := s.State.Guild(guildId)
-		if err != nil {
-			return fmt.Errorf("error retrieving guild %s: %w", guildId, err)
-		}
-		s.State.RLock()
-		defer s.State.RUnlock()
-		rs := make([]*discordgo.Role, 0, len(g.Roles))
+// 	if reorder {
+// 		g, err := s.State.Guild(guildId)
+// 		if err != nil {
+// 			return fmt.Errorf("error retrieving guild %s: %w", guildId, err)
+// 		}
+// 		s.State.RLock()
+// 		defer s.State.RUnlock()
+// 		rs := make([]*discordgo.Role, 0, len(g.Roles))
 
-		for _, role := range g.Roles {
-			if strings.Contains(role.Name, "Elo:") {
-				rs = append(rs, &discordgo.Role{ID: role.ID})
-			}
-		}
+// 		for _, role := range g.Roles {
+// 			if strings.Contains(role.Name, "Elo:") {
+// 				rs = append(rs, &discordgo.Role{ID: role.ID})
+// 			}
+// 		}
 
-		sort.Slice(rs, func(i, j int) bool {
-			matchSize := func(prefix string) aoe4api.TeamSize {
-				switch prefix {
-				case "1v1":
-					return aoe4api.OneVOne
-				case "2v2":
-					return aoe4api.TwoVTwo
-				case "3v3":
-					return aoe4api.ThreeVThree
-				case "4v4":
-					return aoe4api.FourVFour
-				case "Custom":
-					return 5
-				}
-				return 0
-			}
+// 		sort.Slice(rs, func(i, j int) bool {
+// 			matchSize := func(prefix string) aoe4api.TeamSize {
+// 				switch prefix {
+// 				case "1v1":
+// 					return aoe4api.OneVOne
+// 				case "2v2":
+// 					return aoe4api.TwoVTwo
+// 				case "3v3":
+// 					return aoe4api.ThreeVThree
+// 				case "4v4":
+// 					return aoe4api.FourVFour
+// 				case "Custom":
+// 					return 5
+// 				}
+// 				return 0
+// 			}
 
-			iPrefix, jPrefix := strings.Split(rs[i].Name, " ")[0], strings.Split(rs[j].Name, " ")[0]
-			return matchSize(iPrefix) > matchSize(jPrefix)
-		})
+// 			iPrefix, jPrefix := strings.Split(rs[i].Name, " ")[0], strings.Split(rs[j].Name, " ")[0]
+// 			return matchSize(iPrefix) > matchSize(jPrefix)
+// 		})
 
-		for i, role := range rs {
-			role.Position = i
-		}
+// 		for i, role := range rs {
+// 			role.Position = i
+// 		}
 
-		if _, err := s.GuildRoleReorder(guildId, rs); err != nil {
-			return fmt.Errorf("error reordering guild roles: %w", err)
-		}
-	}
+// 		if _, err := s.GuildRoleReorder(guildId, rs); err != nil {
+// 			return fmt.Errorf("error reordering guild roles: %w", err)
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
-func (us users) generateUpdateMessage(st *discordgo.State, guildId string) (string, error) {
-	var updateMessage strings.Builder
-	updateMessage.WriteString("Elo updated!\n\n")
+// func (us users) generateUpdateMessage(st *discordgo.State, guildId string) (string, error) {
+// 	var updateMessage strings.Builder
+// 	updateMessage.WriteString("Elo updated!\n\n")
 
-	for _, u := range us.Users {
-		if u.newElo == nil || fmt.Sprint(u.newElo) == fmt.Sprint(u.oldElo) {
-			continue
-		}
+// 	for _, u := range us.Users {
+// 		if u.newElo == nil || fmt.Sprint(u.newElo) == fmt.Sprint(u.oldElo) {
+// 			continue
+// 		}
 
-		member, err := st.Member(guildId, u.DiscordUserID)
-		if err != nil {
-			return "", fmt.Errorf("error retrieving member %s name: %w", u.DiscordUserID, err)
-		}
+// 		member, err := st.Member(guildId, u.DiscordUserID)
+// 		if err != nil {
+// 			return "", fmt.Errorf("error retrieving member %s name: %w", u.DiscordUserID, err)
+// 		}
 
-		updateMessage.WriteString(fmt.Sprint(member.Mention(), ":\n"))
+// 		updateMessage.WriteString(fmt.Sprint(member.Mention(), ":\n"))
 
-		for _, eloT := range getEloTypes() {
-			if oldElo, newElo := u.oldElo[eloT], u.newElo[eloT]; oldElo != newElo {
-				updateMessage.WriteString(fmt.Sprintln(strings.Title(eloT), "Elo:", oldElo, "->", newElo))
-			}
-		}
+// 		for _, eloT := range getEloTypes() {
+// 			if oldElo, newElo := u.oldElo[eloT], u.newElo[eloT]; oldElo != newElo {
+// 				updateMessage.WriteString(fmt.Sprintln(strings.Title(eloT), "Elo:", oldElo, "->", newElo))
+// 			}
+// 		}
 
-		updateMessage.WriteByte('\n')
-	}
+// 		updateMessage.WriteByte('\n')
+// 	}
 
-	return strings.TrimSpace(updateMessage.String()), nil
-}
+// 	return strings.TrimSpace(updateMessage.String()), nil
+// }
